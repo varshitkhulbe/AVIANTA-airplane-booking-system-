c18caf8f1161c19a09a6498e42b05fa4ce3489a4
varshitkhulbe <varshitkhulbe0304@gmail.com>
2025-04-28 07:56:15 +0530

    created model and migration for airport with foreign key constraint

---
+++ src/models/airport.js
@@ -0,0 +1,49 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class airport extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      // airport.belongsTo(models.City,{
+      //   foreignKey: {
+      //   name:"cityId",
+      //   allowNull: false,
+      //   },
+      //   onDelete: "CASCADE",
+      // })
+    }
+  }
+  airport.init(
+    {
+      name: { 
+              type: DataTypes.STRING, 
+              allowNull: false,
+              unique: true 
+            },
+      code: { 
+              type: DataTypes.STRING, 
+              allowNull: false, 
+              unique: true 
+            },
+      address: {
+              type: DataTypes.STRING, 
+              unique: true 
+               },
+      cityId: { 
+              type: DataTypes.INTEGER, 
+              allowNull: false,
+
+              },
+    },
+    {
+      sequelize,
+      modelName: "airport",
+    }
+  );
+  return airport;
+};
--- src/models/city.js
+++ src/models/city.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      // city.hasMany(models.airport,{
+      //   foreignKey: "cityId"
+      // })
     }
   }
   city.init(
